9d6a724aee0c6d8f057ff95aa2355115039715de
GLgele <GLgele@users.noreply.github.com>
2020-03-17 19:57:34 +0800

    主页GUI化

--- EmployeesDIR/EmployeesDIR.py
+++ EmployeesDIR/EmployeesDIR.py
@@ -1,6 +1,6 @@
 import sys, os
 import dill as pickle
-
+import easygui as gui
 
 #类定义
 class Employee(object):
@@ -108,7 +108,7 @@ choice = 0
 
 #load_data()
 
-
+'''
 while choice != 9:                  
     if sys.platform == "win32":     #清屏
         os.system("cls")
@@ -137,6 +137,101 @@ while choice != 9:
             employees[x].show()
         input("\n按回车继续...")
 
+    elif choice == 2:
+        name = input("\n请输入员工名称: ")
+        sex = input("请输入员工性别:")
+        number = input("请输入员工电话: ")
+        comment = input("请输入员工备注: ")
+        email = input("请输入员工邮箱: ")
+        edu = input("请输入员工学历:")
+        salary = input("请输入员工薪水:")
+        employees.append(Employee(name,sex,number,comment,email,edu,salary))
+        input("\n员工已添加，按回车继续...")
+
+    elif choice == 3:
+        number = int(input("\n请输入员工序号（输入0删除最后一个员工）: "))
+        if number > len(employees):
+            input("错误：没有这个序号！按回车继续...")
+        else:
+            del employees[number - 1]
+            input("\n员工已删除，按回车继续...")
+
+    elif choice == 4:
+        print("\n支持部分查询，如：输入au查找所有 名称 或 电话号码 或 备注 或 邮箱 等信息中含有au的人")
+        search_term = input("请输入员工名称 或 电话号码 或 备注 或 邮箱或其它信息: ")
+        for x in range(0, len(employees)):
+            result = employees[x].find(search_term)
+            if result == 1:
+                print("\n员工序号:", x + 1)
+                employees[x].show()
+            elif result == 0:
+                print("\n没有找到含有该信息的员工！")
+        input("按回车继续...")
+
+    elif choice == 5:
+        filename = input("\n输入文件名（将保存在同一目录）（建议保存为Employees.txt): ")
+        #if filename == "" or filename == " ":
+        #    save_data()
+        save_data(filename)
+
+    elif choice == 6:
+        filename = input("\n输入文件名（请把文件放在同一目录下）（建议保存为Employees.txt): ")
+        #if filename == "" or filename == " ":
+        #    load_data()
+        load_data(filename)
+    elif choice == 7:
+        search_term = input("请输入员工姓名")
+        for x in range(0, len(employees)):
+            result = employees[x].find(search_term)
+            x = x+1
+        if result == 1:
+            print("员工序号：",x)
+            #print("员工姓名：")
+            #Employee.showname(x)
+            print("请选择操作：")
+            print(" 1. 修改姓名")
+            print(" 2. 修改性别")
+            print(" 3. 修改电话")
+            print(" 4. 修改备注")
+            print(" 5. 修改邮箱")
+            print(" 6. 修改学历")
+            print(" 7. 修改薪水")
+            print(" 8. 离开")
+            retval = int(input("请选择操作："))
+            if retval == 8:
+                input("\n按回车继续...")
+            elif retval <8:
+                employees[x-1].setinfo()
+            else:
+                print("错误的操作！")
+                input("\n按回车继续...")
+        elif result == 0:
+            print("\n没有找到含有该信息的员工！")
+    elif choice == 8:
+        print("1. 确定\n 0. 返回")
+        i = input("确定吗？")
+        if i != 0:
+            for x in range(0, len(employees)):
+                del employees[x]
+            input("已删除，按回车继续...")
+        else:
+            input("按回车继续...")
+    elif choice == 9:
+        sys.exit(0)
+    else:
+        print("错误的操作！")
+        input("\n按回车继续...")
+        continue
+
+'''
+
+while choice != 9:
+    choice = gui.indexbox("请选择操作","EmployeesDIR",("查看所有员工","添加员工","删除员工","查找员工","保存员工目录","读取员工目录","修改员工信息","删除所有员工","退出系统")) + 1
+    if choice == 1:
+        for x in range(0, len(employees)):
+            print("\n员工序号:", x + 1)
+            employees[x].show()
+        input("\n按回车继续...")
     elif choice == 2:
         name = input("\n请输入员工名称: ")
         sex = input("请输入员工性别:")
